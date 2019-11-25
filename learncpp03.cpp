1eb792fd506b309c88882e227b12f51519cb5df1
rrsmartinez <rsmartinez@student.apc.edu.ph>
2019-11-25 03:04:25 +0000

    learncpp files

---
+++ learncpp03.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout << "\n\n Find the first 10 natural numbers:\n";
+    cout << "\n";
+    cout << " The natural numbers are: \n";
+    for (num = 1; num <= 10; num++)
+    {
+        cout << num << " ";
+    }
+    cout << endl;
+}
+
---
+++ learncpp04.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+        int a, b;
+        cout << "\n";
+        for(a = 1; a <= 5; a++)
+        {
+        for(b = 1; b <= 5; b++)
+        {
+        cout << a << " ";
+        }
+        cout << endl;
+        }
+        return 0;
+}
+
